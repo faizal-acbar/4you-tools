38e71d9a3da27bdc6897722445cef6fb52c43bde
Extazy <48765638+testbrolah@users.noreply.github.com>
2019-06-17 16:08:37 +0700

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,54 @@
+#
+#    __ __                       __              __    
+#   / // / __  ______  __  __   / /_____  ____  / /____
+#  / // /_/ / / / __ \/ / / /  / __/ __ \/ __ \/ / ___/
+# /__  __/ /_/ / /_/ / /_/ /  / /_/ /_/ / /_/ / (__  ) 
+#   /_/  \__, /\____/\__,_/   \__/\____/\____/_/____/  
+#       /____/            
+#          Hacking Tools by-Faizal Acbar
+
+clear
+
+sudo chmod +x /etc/
+
+clear
+
+sudo chmod +x /usr/share/doc
+
+clear
+
+sudo rm -rf /usr/share/doc/4youtools/
+
+clear
+
+cd /etc/
+
+clear
+
+sudo rm -rf /etc/faizal-acbar
+
+clear
+
+mkdir faizal-acbar
+
+clear
+
+cd faizal-acbar
+
+clear
+
+git clone https://github.com/faizal-acbar/4youtools.git
+
+clear
+
+cd 4youtools
+
+clear
+
+sudo chmod +x install.sh
+
+clear
+
+./install.sh
+
+clear
